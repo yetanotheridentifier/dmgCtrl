7abca88a4c030f079233f4a0e3bee5caceca4744
yetanotheridentifier <44197128+yetanotheridentifier@users.noreply.github.com>
2026-04-26 23:01:19 +0100

    chore/cleanup-files renames and deleted unused files and adds aspect icons for future use

--- vite.config.ts
+++ vite.config.ts
@@ -27,12 +27,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'icon-192.png',
+            src: 'dmgCtrl-icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icon-512.png',
+            src: 'dmgCtrl-icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
